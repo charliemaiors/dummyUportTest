a2209c6c05fd05550e60e5db9fb1af3c822c7db8
carlo.maiorano <pasqualecarlo.maiorano@yahoo.it>
2017-01-17 16:19:14 +0100

    added some stuff

--- uport_interaction.js
+++ uport_interaction.js
@@ -2,14 +2,17 @@
  * Created by maior on 17/01/2017.
  */
 
-import { Uport } from 'uport-lib'
+import {Uport} from 'uport-lib'
 
 let uport = new Uport('TestSiteApp')
 let web3 = uport.getWeb3()
 
-uport.getUserPersona()
-    .then((persona) => {
-        let profile = persona.profile
-        console.log(profile)
-    })
+function login() {
+    uport.getUserPersona()
+        .then((persona) => {
+            let profile = persona.profile
+            console.log(profile)
+        })
+}
+
 
